0dd71e23ae2e2293cc6d654cc4c79e5de3729f66
Lowell Mower & Thomas Goldenberg <pair+lowellmower.tgoldenberg@devbootcamp.com>
2015-06-11 12:02:13 -0400

    add pages and routes for login, logout, and signup with error messages

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,19 +1,38 @@
 get '/login' do
-
+ erb :'auth/login'
 end
 
 get '/signup' do
-
+  erb :'auth/signup'
 end
 
 post '/signup' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/cats'
+  end
+end
 
+get '/cats' do
+  erb :cats
 end
 
 post '/login' do
-
+  @user = User.find_by(username: params[:username])
+  if !@user.nil? && @user.password == params[:password]
+    session[:user_id] = @user.id
+    p session
+    redirect '/'
+  else
+    @messages = ["User name and password do not match"]
+    erb :'auth/login'
+  end
 end
 
-delete '/logout' do
-
+get '/logout' do
+  session[:user_id] = nil
+  redirect back
 end
